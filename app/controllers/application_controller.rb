814d00bd6641cd428da87e4fc686483e96594a3a
kazukiti123 <106233096+kazukiti123@users.noreply.github.com>
2022-07-08 19:29:23 +0900

    Basic認証の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ private
 
 def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '0801'
+      username == 'admin' && password == '2222'
     end
   end
 end
